bd9b4f85f7742afa1180f4b54ca9f127852eedcf
Maxim Shkut <68895847+MaximShkut@users.noreply.github.com>
2023-06-13 20:38:43 +0300

    Add files via upload

---
+++ SolveSum-main/SolveSum/GameConfiguration.swift
@@ -0,0 +1,14 @@
+//
+//  GameConfiguration.swift
+//  SolveSum
+//
+//  Created by Uladzislau Herbed on 13.06.23.
+//
+
+import Foundation
+
+struct GameConfiguration {
+    let maxCellValue = 10
+    let boardSize: Int = 6
+    let maxCellsToSelect = 5
+}
