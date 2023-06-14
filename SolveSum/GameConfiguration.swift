49e2f7cace8c971e7116238c243fc1fa83547927
MaximShkut <m.shkut344@gmail.com>
2023-06-14 18:09:22 +0200

    +

--- SolveSum/GameConfiguration.swift
+++ SolveSum/GameConfiguration.swift
@@ -1,16 +1,14 @@
 //
-//  Configuration.swift
+//  GameConfiguration.swift
 //  SolveSum
 //
 //  Created by user236450 on 6/14/23.
 //
 
-import Foundation{
+import Foundation
 
 struct GameConfiguration {
     let maxCellValue = 10
     let boardSize: Int = 6
     let maxCellsToSelect = 5
-}
-
 }
